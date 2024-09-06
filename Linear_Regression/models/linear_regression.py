8de0b1b16094c59ad44f6d380df03c4f664a7bc3
Alla-Anastasiia Gnatkiv <Alla.Gnatkiv@gmail.com>
2024-09-06 15:15:22 -0400

    changed docs style into Google style docs

--- Linear_Regression/models/linear_regression.py
+++ Linear_Regression/models/linear_regression.py
@@ -3,16 +3,16 @@ import numpy as np
 class LinearRegression():
     """
     Ordinary least squares Linear Regression.
-    
+
     Attributes:
         weights (numpy.ndarray): The weights vector including the bias term.
         coef_ (numpy.ndarray): The weights vector without the bias term.
-        intercept_ (numpy.ndarray): The bias term.
+        intercept_ (float): The bias term.
     """
 
     def __init__(self) -> None:
         """
-        Initializes the LinearRegression model - Ordinary least squares Linear Regression.
+        Initializes the LinearRegression model.
         """
         self.weights = []
         self.coef_ = []
@@ -22,14 +22,17 @@ class LinearRegression():
     def fit(self, X_train, y_train):
         """
         Fits the Linear Regression model to the training data.
-        
+
         This method calculates the weights vector using the ordinary least squares method:
         
             W = (X.T * X)^(-1) * (X.T * y)
-        
+
         Args:
-            X_train (numpy.ndarray): The training data features.
-            y_train (numpy.ndarray): The training data target values.
+            X_train (numpy.ndarray): The training data features with shape (n_samples, n_features).
+            y_train (numpy.ndarray): The training data target values with shape (n_samples,).
+        
+        Returns:
+            None
         """
         X = np.hstack((np.ones((X_train.shape[0], 1)), X_train))
         self.weights = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y_train))
@@ -41,16 +44,16 @@ class LinearRegression():
     def predict(self, X_test):
         """
         Makes predictions using the Linear Regression model.
-        
+
         This method uses the learned weights to predict target values for the test data:
         
-            y_pred = w0 + w1X1 + ... + wnXn =  W * X_augmented
-        
+            y_pred = w0 + w1X1 + ... + wnXn = W * X_augmented
+
         Args:
-            X_test (numpy.ndarray): The test data features.
-        
+            X_test (numpy.ndarray): The test data features with shape (n_samples, n_features).
+
         Returns:
-            numpy.ndarray: The predicted target values.
+            numpy.ndarray: The predicted target values with shape (n_samples,).
         """
         X = np.hstack((np.ones((X_test.shape[0], 1)), X_test))
         y_pred = np.dot(X, self.weights)
